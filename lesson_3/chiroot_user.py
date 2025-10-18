fe924997d6575dd626c7a8bb491ce571d920ba5a
Alexander Erofeev <alexandr.erofeew@gmail.com>
2025-10-18 18:35:14 +0300

    Create folder lesson_3 and add two files with name chiroot_

---
+++ lesson_3/chiroot_user.py
@@ -0,0 +1,15 @@
+#курс Лучший по Python. Часть 2, 15.2
+import random
+
+drop = random.randint(1, 8)
+
+if drop == 1:
+    superpower = 'Заморозка времени'
+elif drop == 2:
+    superpower = 'Невидимость'
+elif drop == 3:
+    superpower = 'Чтение мыслей'
+elif drop == 4:
+    superpower = 'Защитное поле'
+else:
+    print('Увы, ничего не выпало, повезёт в следующий раз!')
