63f2639699fdc739297d9e6f7815c60639f8589e
chiroot <alexandr.erofeew@gmail.com>
2025-10-18 21:27:44 +0300

    Delete second elif in chiroot_user.py

--- lesson_3/chiroot_user.py
+++ lesson_3/chiroot_user.py
@@ -7,7 +7,5 @@ if drop == 1:
     superpower = 'Заморозка времени'
 elif drop == 10:
     superpower = 'Мимикрирование'
-elif drop == 8:
-    superpower = 'Чтение мыслей'
 else:
     print('Увы, ничего не выпало, повезёт в следующий раз!')
