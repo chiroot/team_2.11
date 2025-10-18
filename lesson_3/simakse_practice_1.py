f325ca1c2f916fab15e2ddf8bf70ad2727ac7997
Alexander Erofeev <alexandr.erofeew@gmail.com>
2025-10-18 20:10:13 +0300

    delete if block

--- lesson_3/simakse_practice_1.py
+++ lesson_3/simakse_practice_1.py
@@ -14,13 +14,5 @@ k = 0
 schema = "Scheme add_info_2.json"
 path = "proto.parquet"
 
-#read the parametrs
-if len (argv) > 1:
-    vol = int(argv[1])
-    k = int(argv[2])
-    schema = argv[3]
-    path = argv[4]
-
-
 #generate the data
 #list of receipts atribu
