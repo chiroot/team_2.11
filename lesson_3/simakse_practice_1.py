748dfd6a59f78792fc80b197d58544d30479e2da
simakse <sp.simakov63@gmail.com>
2025-10-18 19:35:14 +0300

    add 2 files as simakse

---
+++ lesson_3/simakse_practice_1.py
@@ -0,0 +1,26 @@
+from sys import argv
+
+import pandas as pd
+import random
+import json
+
+from pyspark.sql.functions import *
+from pyspark.sql import Row, SparkSession
+import pyspark.sql.types as st
+
+#initialize parametrs
+vol = 2000000
+k = 0
+schema = "Scheme add_info_2.json"
+path = "proto.parquet"
+
+#read the parametrs
+if len (argv) > 1:
+    vol = int(argv[1])
+    k = int(argv[2])
+    schema = argv[3]
+    path = argv[4]
+
+
+#generate the data
+#list of receipts atributes
