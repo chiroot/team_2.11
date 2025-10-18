8848a100d0b87d051b24ae506251815535bd5f8a
chiroot <alexandr.erofeew@gmail.com>
2025-10-18 22:11:20 +0300

    Update chiroot_user.py

--- lesson_3/chiroot_user.py
+++ lesson_3/chiroot_user.py
@@ -7,7 +7,9 @@ drop = random.randint(1, 8)
 
 if drop == 1:
     superpower = 'Заморозка времени'
-elif drop == 10:
+elif drop == 2:
+    superpower = 'Мимикрирование'
+elif drop == 3:
     superpower = 'Мимикрирование'
 else:
     print('Увы, ничего не выпало, повезёт в следующий раз!')
